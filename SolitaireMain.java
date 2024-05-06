3342ed6cf38a8676edee51e8da1aa0582e7963bc
vishalashton17 <169017642+vishalashton17@users.noreply.github.com>
2024-05-06 05:09:32 +0000

    I have commited

--- SolitaireMain.java
+++ SolitaireMain.java
@@ -2,6 +2,7 @@
 public class SolitaireMain {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Yay we set up github");
         
     }
 }
