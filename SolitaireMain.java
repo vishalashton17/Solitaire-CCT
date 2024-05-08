33d651b69d63c729bebc69164ca4fe75b3e85f2a
vishalashton17 <169017642+vishalashton17@users.noreply.github.com>
2024-05-08 03:28:51 +0000

    mewssage

--- SolitaireMain.java
+++ SolitaireMain.java
@@ -3,6 +3,7 @@ public class SolitaireMain {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Yay we set up github");
+        System.out.println("Hello again");
         
     }
 }
