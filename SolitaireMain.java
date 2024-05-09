cdc3b3d3b39f79f4647801b12aa7760842bfaf27
vishalashton17 <169017642+vishalashton17@users.noreply.github.com>
2024-05-09 11:18:22 +0800

    Add files via upload

--- SolitaireMain.java
+++ SolitaireMain.java
@@ -1,31 +1,10 @@
+
 public class SolitaireMain {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Yay we set up github");
         System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        System.out.println("Hello again");
-        
         
     }
 }
+    
