1d7898ec83a000f755c440017f82c675c4041d5b
Andyxhr <andyxhr@gmail.com>
2024-05-10 06:22:06 +0800

    Update Spades.java

--- Spades.java
+++ Spades.java
@@ -1,3 +1,19 @@
-public class Spades {
+// Developed by Andy
+public class Spades extends Card {
+    double scoreType;
+    /* To instantiate a Spades object
+    * @param number          - The number of this card
+    * @param pattern         - The pattern of this card
+    * @param color           - The color of this card 
+    * @param scoreType       - The score type of the card, used to calculate score */
+  public Spades( int number, String pattern, String color, double scoreType ) {
+    super(number, pattern, color);
+    this.scoreType = scoreType;
+    }
 
+  public int CalcScore() {
+    int score;
+    score = (int)Math.pow(scoreType + 10, 2 );
+    return score;
+  }
 }
