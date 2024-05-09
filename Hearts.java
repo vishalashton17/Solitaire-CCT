7f145859ce8b60770730bdc999ec81363d8e7fb6
Andyxhr <andyxhr@gmail.com>
2024-05-10 06:21:54 +0800

    Update Hearts.java

--- Hearts.java
+++ Hearts.java
@@ -1,3 +1,19 @@
-public class Hearts {
+// Developed by Andy
+public class Hearts extends Card {
+    int scoreType;
+    /* To instantiate a Hearts object
+    * @param number          - The number of this card
+    * @param pattern         - The pattern of this card
+    * @param color           - The color of this card 
+    * @param scoreType       - The score type of the card, used to calculate score */
+  public Hearts( int number, String pattern, String color, int scoreType ) {
+    super(number, pattern, color);
+    this.scoreType = scoreType;
+    }
 
+  public int CalcScore() {
+    int score;
+    score = (int)Math.pow(scoreType + 10, 2 );
+    return score;
+  }
 }
