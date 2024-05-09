90cceb51de62447fd563c33ba6c0242d5604b9c8
Andyxhr <andyxhr@gmail.com>
2024-05-10 06:21:34 +0800

    Update Diamonds.java

--- Diamonds.java
+++ Diamonds.java
@@ -1,3 +1,19 @@
-public class Diamonds {
+// Developed by Andy
+public class Diamonds extends Card {
+    String scoreType;
+    /* To instantiate a Diamonds object
+    * @param number          - The number of this card
+    * @param pattern         - The pattern of this card
+    * @param color           - The color of this card 
+    * @param scoreType       - The score type of the card, used to calculate score */
+  public Diamonds( int number, String pattern, String color, String scoreType ) {
+    super(number, pattern, color);
+    this.scoreType = scoreType;
+    }
 
+  public int CalcScore() {
+    int score;
+    score = scoreType.length();
+    return score;
+  }
 }
