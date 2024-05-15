181528d6a0c516e1c9f2a5b1fe41f0410e442caf
Andyxhr <andyxhr@gmail.com>
2024-05-15 10:50:31 +0800

    Update Clubs.java

--- Clubs.java
+++ Clubs.java
@@ -10,7 +10,9 @@ public class Clubs extends Card {
     super(number, pattern, color);
     this.scoreType = scoreType;
     }
-
+    /* Get the Card color
+    * @param scoreType - The score type of the card, used to calculate score
+    * @return          - The score */
   public int CalcScore(char scoreType) {
     int score;
     score = scoreType/10;
