d69068c446c65581fae5023e05575913f6ef31f8
Andyxhr <andyxhr@gmail.com>
2024-05-20 10:53:32 +0800

    Update Displayer.java

--- Displayer.java
+++ Displayer.java
@@ -33,18 +33,51 @@ public class Displayer {
     /* Displays the game board on the screen
      * @param board - The 2D array that contains Card objects representing the board */
     public void DisplayBoard(Card[][] board) {
-
+        System.out.print("BOARD");
+        System.out.print("==============================================");
+        for(int i = 0; i<13 ; i++){
+            System.out.println(i);
+            for (int k = 0; k<7; k++){
+                if (board[i][k] == null){
+                    System.out.print(k +" EMPTY " +i);
+                }
+                else {
+                System.out.println(board[i][k].GetNumber());
+                System.out.println(board[i][k].GetColor());
+                System.out.println(board[i][k].GetPattern());
+                System.out.print("------------------------------------------------");
+                }
+            }
+        }
     }
     
     /* Displays the stockpile on the screen
      * @param stockpile - The array that contains Card objects representing the stockpile */
-    public void DisplayStockpile(Card[] stockpile) {
-      
+    public void DisplayStockpile(Card[] stockpile, int currCardPos) {
+      System.out.print("STOCKPILE");
+      System.out.println(stockpile[currCardPos].GetNumber());
+      System.out.println(stockpile[currCardPos].GetColor());
+      System.out.println(stockpile[currCardPos].GetPattern());
+
     }
     
     /* Displays the store on the screen
      * @param store - The array that contains Card objects representing the store */
     public void DisplayWaste(Card[][] waste) {
-      
+        System.out.print("WASTE");
+        System.out.print("==============================================");
+        for(int i = 0; i<13 ; i++){
+            System.out.println("");
+            for (int k = 0; k<4; k++){
+                if (waste[i][k] == null){
+                    System.out.print(k +" EMPTY " +i);
+                }
+                else{
+                System.out.println(waste[i][k].GetNumber());    
+                System.out.println(waste[i][k].GetColor());
+                System.out.println(waste[i][k].GetPattern());
+                }
+            }
+        }
     }
   }
