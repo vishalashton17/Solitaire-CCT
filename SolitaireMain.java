a641ac74c2a1855d94ec8647d06a7936a68c747a
vishalashton17 <169017642+vishalashton17@users.noreply.github.com>
2024-05-26 13:02:27 +0800

    Update SolitaireMain.java

--- SolitaireMain.java
+++ SolitaireMain.java
@@ -2,58 +2,76 @@
 import java.util.Scanner;
 
 public class SolitaireMain {
-    // The start of the game
-    public static void main(String[] args) {
-        // Creates the Displayer object
-        Displayer displayer = new Displayer();
-        
-        // Creates the Solitaire object
-        Solitaire sol;
+public static void main(String[] args) {
+    // Creates the Displayer object
+    Displayer displayer = new Displayer();
+    
+    // Creates the Solitaire object
+    Solitaire sol;
 
-        // Create scanner object for user input
-        Scanner input = new Scanner(System.in);
+    // Create scanner object for user input
+    Scanner input = new Scanner(System.in);
 
-        // Declared and initialised all variables
-        int rounds = 0;
+    // Declared and initialised all variables
+    String player1, player2;
+    
 
-        displayer.GameHeader(); // Displays game header
+    displayer.GameHeader(); // Displays game header
 
-        sol = new Solitaire(); // Initialize Solitaire object
-
-        System.out.println("initalizing game");
-        // Initialize the game
-        sol.InitializeGame();
-
-
+    sol = new Solitaire(); // Initialize Solitaire object
+    
+    System.out.println("Initializing game");
+   
+    System.out.println("Enter first player's name:"); 
+    player1 = input.nextLine();
+    System.out.println("Enter second player's name:"); 
+    player2 = input.nextLine();
+    
+    // Initialize the game
+    sol.InitializeGame(player1, player2);
+    
+    while(!sol.CheckIfWon()) {
         // Display the initial game state
         displayer.DisplayBoard(sol.GetBoard());
         displayer.DisplayStockpile(sol.GetStockpile(), sol.GetCurrStockpilePos());
         displayer.DisplayWaste(sol.GetWaste());
 
+        // Get and display current player
+        String currPlayer = sol.GetCurrPlayer();
+        System.out.println("Current Player: " + currPlayer);
+
         // Movements within the 7 piles
         System.out.println("Do you want to move cards within the piles of cards? (Y/N)");
-        char moveCards  = input.next().charAt(0);
-        int fromPile;
-        int toPile;
-        if(moveCards == 'y' || moveCards == 'Y'){
-            do{
-                System.out.println("Move card from pile number(Key in 0 if proceeding to stockpile): "); 
+        char moveCards = input.next().charAt(0);
+        int fromPile, toPile;
+        if(moveCards == 'y' || moveCards == 'Y') {
+            do {
+                System.out.println("Move card from pile number (Enter 0 to proceed to stockpile): "); 
                 fromPile = input.nextInt();
+                input.nextLine(); // newline
 
+                if(fromPile == 0) {
+                  break;
+                }
+                
                 System.out.println("Move card to pile number: ");
                 toPile = input.nextInt();
+                input.nextLine(); //newline
 
                 sol.BoardCardMovement(fromPile, toPile);
-            }
-            while(fromPile == 0);
-            
+            } while(fromPile == 0);
         }
 
-        System.out.println("Where do you want to place the stockpile card?(Column 1-7, 8 to move to waste)");
+        System.out.println("Where do you want to place the stockpile card? (Column 1-7, 8 to move to waste)");
         int stockpileMove = input.nextInt();
-
         sol.StockpileCardMovement(stockpileMove);
-
-        input.close();
+        
+        sol.SwitchPlayer();
     }
+// Game is won
+    boolean winner = sol.CheckIfWon();
+    System.out.println("Congratulations " + winner + "! You've won the game!");
+
+    input.close();
+}
 }
