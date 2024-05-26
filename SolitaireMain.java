6056221aeded5dabd590fd27cd3a6052eb3d24fb
vishalashton17 <169017642+vishalashton17@users.noreply.github.com>
2024-05-26 18:08:23 +0800

    Update SolitaireMain.java

--- SolitaireMain.java
+++ SolitaireMain.java
@@ -19,8 +19,6 @@ public static void main(String[] args) {
     displayer.GameHeader(); // Displays game header
 
     sol = new Solitaire(); // Initialize Solitaire object
-    
-    System.out.println("Initializing game");
    
     System.out.println("Enter first player's name:"); 
     player1 = input.nextLine();
@@ -28,48 +26,68 @@ public static void main(String[] args) {
     player2 = input.nextLine();
     
     // Initialize the game
-    sol.InitializeGame(player1, player2);
+    sol.SetPlayers(player1, player2);
+    sol.InitializeGame();
     
     while(!sol.CheckIfWon()) {
         // Display the initial game state
+        displayer.DisplayBoard(sol.GetBoard());
         displayer.DisplayStockpile(sol.GetStockpile(), sol.GetCurrStockpilePos());
         displayer.DisplayWaste(sol.GetWaste());
-        displayer.DisplayBoard(sol.GetBoard());
 
         // Get and display current player
         String currPlayer = sol.GetCurrPlayer();
         System.out.println("Current Player: " + currPlayer);
 
         // Movements within the 7 piles
+        char moveCards;
         System.out.println("Do you want to move cards within the piles of cards? (Y/N)");
-        char moveCards = input.next().charAt(0);
+        moveCards = input.next().charAt(0);
         int fromPile, toPile;
-        if(moveCards == 'y' || moveCards == 'Y') {
-            do {
-                System.out.println("Move card from pile number (Enter 0 to proceed to stockpile): "); 
+        while(moveCards == 'y' || moveCards == 'Y') {
+                System.out.println("Move card from pile number: "); 
                 fromPile = input.nextInt();
                 input.nextLine(); // newline
-
-                if(fromPile == 0) {
-                  break;
-                }
                 
                 System.out.println("Move card to pile number: ");
                 toPile = input.nextInt();
                 input.nextLine(); //newline
 
-                sol.BoardCardMovement(fromPile, toPile);
-            } while(fromPile == 0);
-        }
+                int boardMovement = sol.BoardCardMovement(fromPile-1, toPile-1);
+                if (boardMovement == -1) {
+                  System.out.println("ERROR: Invalid column numbers!");    
+                }
+                else if (boardMovement == 1) {
+                  System.out.println("Card moved successfully");  
+                }
+                else {
+                  System.out.println("ERROR: Invalid move!");
+                }
+               System.out.println("Do you want to continue move cards within the piles of cards? (Y/N)");
+               moveCards = input.next().charAt(0);
+            }
+        
 
         System.out.println("Where do you want to place the stockpile card? (Column 1-7, 8 to move to waste)");
         int stockpileMove = input.nextInt();
+
         sol.StockpileCardMovement(stockpileMove);
-        
+        int stockpileToBoardMovement = sol.StockpileCardMovement(stockpileMove-1);
+                if (stockpileToBoardMovement == -1) {
+                  System.out.println("ERROR: Invalid column numbers!");    
+                }
+                else if (stockpileToBoardMovement == 1) {
+                  System.out.println("Card moved successfully");  
+                  sol.AddScore();
+                }
+                else {
+                  System.out.println("ERROR: Invalid move!");
+                }
+                
         sol.SwitchPlayer();
     }
 // Game is won
-    boolean winner = sol.CheckIfWon();
+    String winner = sol.GetWinner();
     System.out.println("Congratulations " + winner + "! You've won the game!");
 
     input.close();
