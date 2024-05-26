a5f2445ed7add8c438d488de393a47b3a2961920
Andyxhr <andyxhr@gmail.com>
2024-05-26 22:03:53 +0800

    Update Solitaire.java

--- Solitaire.java
+++ Solitaire.java
@@ -8,9 +8,10 @@ public class Solitaire {
     private Card[][] waste; // Array to represent the waste pile
     private Card[] stockpile; // Array to represent the stockpile
     private Player[] allPlayers; // An array that stores all the player objects
+    private int round; // Keeps track of the current round
     private int currCardPos; // Keeps track of the current card position
-    private Player currPlayer;         // Keeps track of the current player
     private Card[] temp; // Array to store cards when they are first created
+    private int currPlayer; // Keeps track of the current player array position
 
     //======================= CONSTRUCTOR =======================//
     // Initialize the game's instance variables. 
@@ -20,6 +21,7 @@ public class Solitaire {
         waste = new Card[13][4];
         stockpile = new Card[24];
         temp = new Card[52];
+        allPlayers = new Player[2];
     }
 
     //====================== PRIVATE METHOD =======================//
@@ -51,18 +53,97 @@ public class Solitaire {
         }
         return false; // Card not found in the foundation pile
     }
+    // Randomizes the cards in the stockpile 
+    private void RandomizeStockpile() {
+        // Shuffle the remaining cards in the temp array
+        Random rand = new Random();
+        int count = 0;
+        boolean run = true;
 
-    //====================== PUBLIC METHOD =======================//
+        // Collect non-null cards
+        Card[] remainingCards = new Card[28];
+        while (run){
+          for (int i = 0; i < 52; i++){
+            if (temp[i] != null && count < 28) {
+                remainingCards[count] = temp[i];
+                count ++;
+            }
+            run = false;
+          }
+        }
 
-    public void SetPlayers(String player1, String player2){
-        allPlayers = new Player[2];
-        allPlayers[0] = new Player(player1);
-        allPlayers[1] = new Player(player2);
-        currPlayer = allPlayers[0];
+        // Shuffle the remaining cards
+        for (int i = remainingCards.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+            Card tempCard = remainingCards[index];
+            remainingCards[index] = remainingCards[i];
+            remainingCards[i] = tempCard;
+        }
+
+        count = 0;
+        for (int col = 0; col < 7; col++) {
+            for (int row = 0; row <= col; row++) {
+                for (int i = 0; i < 52; i++) {
+                    if (temp[i] != null) {
+                        board[row][col] = temp[i];
+                        temp[i] = null;
+                        break;
+                    }
+                }
+                board[row][col] = remainingCards[count++];
+            }
+        }
+
     }
 
+    private void RandomizeBoard() {
+        for (int col = 0; col < 7; col++) {
+            for (int row = 0; row <= col; row++) {
+                for (int i = 0; i < 52; i++) {
+                    if (temp[i] != null) {
+                        board[row][col] = temp[i];
+                        temp[i] = null;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /* Removes a card from the stockpile
+     * @param card - The card to be removed */
+    private void RemoveCardFromStockpile(Card card) {
+        for (int i = 0; i < 24; i++) {
+            if (stockpile[i] != null){
+                if (card.GetPattern().equals(stockpile[i].GetPattern()) && card.GetNumber() == stockpile[i].GetNumber()) {
+                    allPlayers[currPlayer].AddScore(stockpile[i].CalcScore());
+                    stockpile[i] = null;
+                    System.out.println("success");
+                }
+            }
+        }
+    }
+    // Checks if the current stockpile is empty and moves to the next card if it is
+    private void CheckStockpile(){
+        int pos = 0;
+        for (int i = 0; i < 24; i++) {
+            if (stockpile[currCardPos] != null){
+                break;
+            }
+            else if (stockpile[pos] == null){
+                currCardPos++;
+                break;
+            }
+        }
+    }
+
+
+    //====================== PUBLIC METHOD =======================//
     // Initializes the game
-    public void InitializeGame() {
+    public void InitializeGame(String player1, String player2) {
+        allPlayers[0] = new Player(player1);
+        allPlayers[1] = new Player(player2);
+        
         for (int i = 0; i < 4; i++) {
             for (int k = 0; k < 13; k++) {
                 if (i == 0) {
@@ -89,241 +170,203 @@ public class Solitaire {
         board[4][4].SetFacing();
         board[5][5].SetFacing();
         board[6][6].SetFacing();
-        
 
 
     }
 
 
-    // Randomizes the cards in the stockpile 
-    public void RandomizeStockpile() {
-        Random rand = new Random();
-        int index;
-        boolean[] used = new boolean[52];
-        Arrays.fill(used, false);
-
-        for (int i = 0; i < 24; i++) {
-            do {
-                index = rand.nextInt(52);
-            } while (used[index]);
-
-            used[index] = true;
-            AddCardToStockpile(temp[index]);
-        }
-    }
-
-    public void RandomizeBoard() {
-        for (int col = 0; col < 7; col++) {
-            for (int row = 0; row <= col; row++) {
-                for (int i = 0; i < 52; i++) {
-                    if (temp[i] != null) {
-                        board[row][col] = temp[i];
-                        temp[i] = null;
-                        break;
+    /* Checks if the game is won
+        * @return - true if the game is won
+                - false otherwise */
+        public boolean CheckIfWon() {
+            // check the 7 piles
+            for (int suit = 0; suit < 4; suit++) {
+                //  foundation pile has all cards from Ace to King?
+                for (int rank = 1; rank <= 13; rank++) {
+                    if (!IsCardInPiles(rank, suit)) {
+                        return false; // 
                     }
                 }
             }
+            return true; // All cards are in the foundation piles
         }
-    }
 
-    // Flips the top card from the stockpile to the waste pile 
-    public void StockpileToWaste() {
-        if (stockpile[currCardPos] == null) {
 
-        } else if (stockpile[currCardPos].GetNumber() == 1) {
-            for (int k = 0; k < 4; k++) {
-                if (waste[0][k] == null) {
-                    waste[0][k] = stockpile[currCardPos];
-                    RemoveCardFromStockpile(stockpile[currCardPos]);
-                    break;
+    /*  Moves the current stockpile card into the waste pile.
+        @return - true if card is moved succesffuly
+                - false if card is not moved */
+    public boolean StockpileToWaste() {
+        int count = 0;
+        while (count < 25) {
+            System.out.println(count);
+            if (stockpile[currCardPos] == null ) {
+                System.out.println("ended333333333333333 sadsadsadasddsa");
+                System.out.println(count);
+                CheckStockpile();
+            } else if (stockpile[currCardPos].GetNumber() == 1) {
+                System.out.println("ended sadsadsadasddsa");
+                for (int k = 0; k < 4; k++) {
+                    System.out.println("ended4444 sadsadsadasddsa");
+                    if (waste[0][k] == null) {
+                        System.out.println("ended 5555555sadsadsadasddsa");
+                        waste[0][k] = stockpile[currCardPos];
+                        System.out.println(waste[0][k].GetPattern());
+                        RemoveCardFromStockpile(stockpile[currCardPos]);
+                        CheckStockpile();
+                        round ++;
+                        return true;
+                    }
                 }
             }
+            else if (stockpile[currCardPos].GetNumber() > 1 ) {
+                for (int k = 0; k < 4; k++) {
+                    for (int i = 1; i < 13; i++) {
+                        if (waste[i][k] == null && waste[i-1][k] != null) {
+                            System.out.println("ended 111111111sadsadsadasddsa");
+                            if (stockpile[currCardPos].GetPattern() == waste[i-1][k].GetPattern()) {
+                                System.out.println("ended 2222222sadsadsadasddsa");
+                            waste[i][k] = stockpile[currCardPos];
+                            RemoveCardFromStockpile(stockpile[currCardPos]);
+                            CheckStockpile();
+                            round ++;
+                            return true;
+                            }
+                        }
+                    }
+                }
+            }
+            count ++;
+            System.out.println(count);
         }
+        System.out.println("ended StockpileToWaste");
+        return false;
     }
 
-    /* Adds a card to the stockpile
-     * @param card - The card to be added */
-    public void AddCardToStockpile(Card card) {
-        for (int i = 0; i < 24; i++) {
-            if (stockpile[i] == null) {
-                stockpile[i] = card;
-                break;
+    
+
+
+    /* The movement of the card within the board
+    * @return - -1 if the column number is invalid
+              - 0 if the movement is not complete
+              - 1 if the movement is complete 
+              -2 if coloumn is invalid*/
+   public int BoardCardMovement(int fromPile, int toPile) {
+    // Check if the column numbers are valid
+    if (fromPile < 0 || fromPile >= 7 || toPile < 0 || toPile >= 7) {
+        return -2; // Invalid column numbers
+    }
+    
+    // Check if there are cards in the source pile
+    if (board[0][fromPile] == null) {
+        return -1; // Source pile is empty
+    }
+    
+    // Check if the destination pile is empty or if the move is valid
+    if (board[0][toPile] == null || board[0][toPile].CanMoveOnto(board[0][fromPile])) {
+        // Move the card
+        for (int row = 12; row >= 0; row--) {
+            if (board[row][fromPile] != null) {
+                board[row][toPile] = board[row][fromPile];
+                board[row][fromPile] = null;
+                return 1; // Movement successful
             }
         }
+    } else {
+        return 0; // Invalid move
     }
+    
+    return 0; // Unable to move the card
+}
+   
 
-    /* Removes a card from the stockpile
-     * @param card - The card to be removed */
-    public void RemoveCardFromStockpile(Card card) {
-        for (int i = 0; i < 24; i++) {
-            if (card.GetPattern().equals(stockpile[i].GetPattern()) && card.GetNumber() == stockpile[i].GetNumber()) {
-                stockpile[i] = null;
+    /* The movement of the card from the stockpile
+    * @return -  -1 if the column number is invalid
+                 -0 if the movement is not complete
+                 -1 if the movement is complete 
+                 -2 if Stockpile is empty */
+   public int StockpileCardMovement(int toPile) {
+    // Check if the column number is valid
+    if (toPile < 0 || toPile >= 7) {
+        return -1; // Invalid column number
+    }
+    
+    // Check if the stockpile is empty
+    if (stockpile[currCardPos] == null) {
+        return -2; // Stockpile is empty
+    }
+    
+    // Check if the destination pile is empty or if the move is valid
+    if (board[0][toPile] == null || board[0][toPile].CanMoveOnto(stockpile[currCardPos])) {
+        // Move the card from the stockpile to the board
+        for (int row = 12; row >= 0; row--) {
+            if (board[row][toPile] == null) {
+                board[row][toPile] = stockpile[currCardPos];
+                stockpile[currCardPos] = null;
+                currCardPos++;
+                return 1; // Movement successful
             }
         }
+    } else {
+        return 0; // Invalid move
+    }
+    return 0; // Unable to move the card
+}
+
+
+
+    /* Adds score to the game */
+    public void AddScore() {
+
     }
 
+    /* Switch player's turn to the next player (If current player is player 1, then it 
+    * switches to player 2. If current player is player 2, then it switches to player 1) */   
+    public void SwitchPlayer() {
+    if (allPlayers[currPlayer] == allPlayers[0]) {
+    currPlayer = 1;
+    } 
+    else {
+    currPlayer = 0;
+    }
+  }
     /* Get the current card from the stockpile
      * @return - The current card from the stockpile */
     public int GetCurrStockpilePos() {
         return currCardPos;
     }
 
-    /* Moves a card from the store to the stockpile
-     * @param card - The card to be moved
-     * @return - The number of points gained */
-    public int MoveCardFromStore(Card card) {
-        return 0;
-    }
-
-    /* Checks if the game is won
-     * @return - true if the game is won
-               - false otherwise */
-    public boolean CheckIfWon() {
-        // check the 7 piles
-        for (int suit = 0; suit < 4; suit++) {
-            //  foundation pile has all cards from Ace to King?
-            for (int rank = 1; rank <= 13; rank++) {
-                if (!IsCardInPiles(rank, suit)) {
-                    return false; // 
-                }
-            }
-        }
-        return true; // All cards are in the foundation piles
-    }
-
-
-    /* The movement of the card within the board
-    * @return - -1 if the column number is invalid
-              - 0 if the movement is not complete
-              - 1 if the movement is complete */
-              public int BoardCardMovement(int fromPile, int toPile) {
-                System.out.println("1- " + board[0][fromPile] + "2- " + board[0][toPile]);
-               // Check if the column numbers are valid
-               if (fromPile < 0 || fromPile >= 7 || toPile < 0 || toPile >= 7) {
-                   return -1; // Invalid column numbers
-               }
-               
-               // Check if there are cards in the source pile
-               if (board[0][fromPile] == null) {
-                   return -1; // Source pile is empty
-               }
-               
-               // Check if the destination pile is empty or if the move is valid
-               System.out.println("1- " + board[0][fromPile] + "2- " + board[0][toPile]);
-               if (board[12][toPile] == null || board[12][toPile].CanMoveOnto(board[0][fromPile], board[12][toPile])) {
-                   // Move the card
-                 System.out.println("Called card");
-                   for (int row = 12; row >= 0; row--) {
-                       if (board[row][fromPile] != null) {
-                           board[row][toPile] = board[row][fromPile];
-                           board[row][fromPile] = null;
-                           return 1; // Movement successful
-                       }
-                   }
-               } else {
-                   return 0; // Invalid move
-               }
-               
-               return 0; // Unable to move the card
-           }
-   
-
-    /* The movement of the card from the stockpile
-    * @return - -1 if the column number is invalid
-              - 0 if the movement is not complete
-              - 1 if the movement is complete */
-              public int StockpileCardMovement(int toPile) {
-                // Check if the column number is valid
-                if (toPile < 0 || toPile >= 7) {
-                    return -1; // Invalid column number
-                }
-                
-                // Check if the stockpile is empty
-                if (stockpile[currCardPos] == null) {
-                    return -1; // Stockpile is empty
-                }
-                
-                // Check if the destination pile is empty or if the move is valid
-                if (board[0][toPile] == null || board[0][toPile].CanMoveOnto(stockpile[currCardPos], board[0][toPile])) {
-                    // Move the card from the stockpile to the board
-                    for (int row = 12; row >= 0; row--) {
-                        if (board[row][toPile] == null) {
-                            board[row][toPile] = stockpile[currCardPos];
-                            stockpile[currCardPos] = null;
-                            currCardPos++;
-                            return 1; // Movement successful
-                        }
-                    }
-                } else {
-                    return 0; // Invalid move
-                }
-            
-                return 0; // Unable to move the card
-            }
-
-
-    /* Adds score to the game */
-    public void AddScore() {
-        String pattern = stockpile[currCardPos].GetPattern();
-        int score = 0;
-        if (pattern.equals("diamond")) {
-            score = 10;
-        } else if (pattern.equals("heart")) {
-            score = 15;
-        } else if (pattern.equals("spade")) {
-            score = 20;
-        } else if (pattern.equals("club")) {
-            score = 25;
-        } else {
-            score = 5;
-        }
-
-        currPlayer.AddScore(score);
-    
-}
-
-    /* Switch player's turn to the next player (If current player is player 1, then it 
-    * switches to player 2. If current player is player 2, then it switches to player 1) */   
-    public void SwitchPlayer() {
-    if (currPlayer == allPlayers[0]) {
-        currPlayer = allPlayers[1]; // Switch to player 2
-    } 
-    else {
-        currPlayer = allPlayers[0]; // Switch to player 1
-    }
-}
-  
-  
-public String GetWinner() {
-    
-    int player1Score = allPlayers[0].GetScore();
-    int player2Score = allPlayers[1].GetScore();
-
-    if (player1Score > player2Score) {
-        return allPlayers[0].GetName(); 
-    } 
-    else if (player1Score < player2Score) {
-        return allPlayers[1].GetName(); 
-    } 
-    else {
-        return "Tie"; 
-    }
-}
-
+    /* Get the board 2d array
+     * @return - The board 2d array */
     public Card[][] GetBoard() {
         return board;
     }
+
+    /* Get the waste 2d array
+     * @return - The waste 2d array */
     public Card[][] GetWaste() {
         return waste;
     }
+
+    /* Get the stockpile array
+     * @return - The stockpile array */
     public Card[] GetStockpile() {
         return stockpile;
     }
+
+    /* Get the array with all player data
+     * @return - The allPlayer array */
     public Player[] GetPlayers() {
         return allPlayers;
     }
-    public String GetCurrPlayer() {
-        return currPlayer.GetName();
+    /* Get the current round
+     * @return - The current card from the stockpile */
+    public int GetRound() {
+        return round;
+    }
+    /* Get the current player
+     * @return - The current player */
+    public Player GetCurrPlayer() {
+        return allPlayers[currPlayer];
     }
 
 }
