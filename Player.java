8b581eae0c74548f8349ef966a1518b6f21ae418
vishalashton17 <169017642+vishalashton17@users.noreply.github.com>
2024-05-26 13:00:43 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,31 +1,39 @@
-// Developed by Andy
 public class Player {
-    private String name;
-    private int score;
-   
-   //======================= CONSTRUCTOR =======================//
-   /* @discTpe - The type of disc that is being created (Either O or X) */
-   public Player(String name) {
-     this.name = name;
-   }
-   
-   //====================== PUBLIC METHOD =======================//
-   /* Gets the player's name
-    * @return - The name of the player */
-   public String GetName() {
-     return name;
-   }
-   
-   /* Gets the players score 
-    * @return - The player's score */
-   public int GetScore() {
-     return score;
-   }
-   
-   /* Add scores to the player's exiting score
-    * @param score - The score that needs to be added
-    *                to the player's score */
-   public void AddScore(int score) {
-     this.score += score;
-   }
+  private String name;
+  private int score;
+
+ //======================= CONSTRUCTOR =======================//
+ /* @discTpe - The type of disc that is being created (Either One or Two) */
+
+ // Developed by Vishal
+ public Player(String playerName) {
+  this.name = playerName; // Assigning playerName to instance variable this.name
+  this.score = 0;
+}
+ 
+ //====================== PUBLIC METHODS =======================//
+ /* Gets the player's name
+  * @return - The name of the player */
+
+ // Developed by Andy
+ public String GetName() {
+   return name;  
  }
+ 
+ /* Gets the players score 
+  * @return - The player's score */
+ 
+ // Developed by Vishal
+ public int GetScore() {
+   return score;
+ }
+ 
+ /* Add scores to the player's exiting score
+  * @param score - The score that needs to be added
+  *                to the player's score */
+
+ // Developed by Andy
+ public void AddScore(int score) {
+   this.score += score;
+ }
+}
